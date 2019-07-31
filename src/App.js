4766853a872e9c72099f2ef8a3edeb7ec41eecaa
Allan Cheng <allanhfcheng@gmail.com>
2019-07-30 21:57:13 -0700

    for session

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import Search from './Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      term: "",
+      printtype: "",
+      booktype: ""
+
+    };
+  }
+ 
+  setBooks(books){
+    this.setState({
+      books
+    });
+  }
+
+  setTerm(term) {
+    this.setState({
+      term
+    });
+  }
+
+  setprintType(printtype) {
+    this.setState({
+      printtype
+    });
+  }
+
+  setbookType(booktype) {
+    this.setState({
+      booktype
+    });
+  }
+
+
+
+  render() {
+    console.log(this.state.books);
+    const page = 
+    <Search  setTerm ={term => this.setTerm(term)} setprintType ={printtype => this.setprintType(printtype)}  setbookType ={booktype => this.setbookType(booktype)} setbooks ={books => this.setBooks(books)} />
+    
+    /* this.state.showAddForm
+          ? <AddBookmark 
+                 showForm={show => this.setShowAddForm(show)} 
+                 handleAdd={bookmark => this.addBookmark(bookmark)}/>
+          : <BookmarkApp bookmarks={this.state.bookmarks} showForm={show => this.setShowAddForm(show)}/>; */
+
+    return (
+      <div className="App">
+        { page }
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ src/Search.js
@@ -0,0 +1,154 @@
+import React, { Component } from  'react';
+//import './addBookmark.css';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      url: "",
+      description: "",
+      rating: 1
+    };
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+
+
+  fetch("https://www.googleapis.com/books/v1/volumes?q=flowers&filter=free-ebooks&key=AIzaSyChQCwLNv3ZWkHi8OG5_UUszmgr9iguzHw", {
+      method: 'GET',
+      headers: {
+        "Accept": "application/json",
+      }
+    }).then((res)=>res.json())
+    .then((json)=>{
+      this.props.setbooks(json.items);
+    })
+  }
+
+  changeSelection(value) {
+   
+      this.props.setTerm(value);
+    }
+
+    changePrint(value) {
+   
+      this.props.setprintType(value);
+    }
+    changeBook(value) {
+      this.props.setbookType(value);
+    }
+
+    render() {
+      
+       return(
+         <div className="addbookmark">
+           <h2>Add Bookmark</h2>
+           <form className="addbookmark__form" onSubmit={e => this.handleSubmit(e)}>
+             <label htmlFor="title">Title:</label>
+             <input 
+               type="text" 
+               name="title" 
+               id="title" 
+               placeholder="Title"
+               value={this.state.title}
+               onChange={e => this.changeSelection(e.target.value)}/>
+           <label htmlFor="printType">Print Type</label>
+             <select
+               id="printType"
+               name="printType"
+               onChange={e => this.changePrint(e.target.value)}>
+               <option value="all">All</option>
+               <option value="books">Books</option>
+               <option selected value="magazine">Magazine</option>
+             </select>
+             <label htmlFor="bookType">Book Type</label>
+             <select
+               id="bookType"
+               name="bookType"
+               onChange={e => this.changeBook(e.target.value)}>
+               <option value="partial">partial</option>
+               <option value="full">full</option>
+               <option value="free-ebooks">free-ebooks</option>
+               <option value="paid-ebooks">paid-ebooks</option>
+               <option value="ebooks">ebooks</option>
+             </select>
+             <div className="addbookmark__buttons">
+               <button onClick={e => this.props.showForm(false)}>Cancel</button>
+               <button type="submit" >Save</button>
+             </div>  
+           </form>
+         </div>
+       );
+     }
+
+
+  }
+
+  
+
+  /*
+  titleChanged(title) {
+    this.setState({
+      title
+    });
+  }
+
+  urlChanged(url) {
+    this.setState({
+      url
+    });
+  }
+
+  descriptionChanged(description) {
+    this.setState({
+      description
+    });
+  }
+
+  ratingChanged(rating) {
+    this.setState({
+      rating
+    });
+  }*/
+/*
+  handleSubmit(e) {
+    e.preventDefault();
+    const bookmark = (({title, url, description, rating}) => ({title, url, description, rating}))(this.state);
+    const url ='https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks';
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(bookmark),
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer $2a$10$IcHIjHNXPYmFCPdWOjRWGefs/54EFAZwZcW7H5MEZHK1HhGSWT.Ti"
+      }
+    };
+
+    fetch(url, options)
+      .then(res => {
+        if(!res.ok) {
+          throw new Error('Something went wrong, please try again later');
+        }
+        return res.json();
+      })
+      .then(data => {
+        this.setState({
+          title: "",
+          url: "",
+          description: "",
+          rating: 1
+        });
+        this.props.handleAdd(bookmark);
+      })
+      .catch(err => {
+        this.setState({
+          error: err.message
+        });
+      });
+  }
+*/
+ 
+
+
+export default Search;
